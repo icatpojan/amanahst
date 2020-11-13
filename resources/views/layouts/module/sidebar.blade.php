eb5b91f53cf496c6334245d4a2fc9b0da21f951d
icatpojan <irsyadfauzangg@gmail.com>
2020-11-13 16:20:27 +0700

    perubahan kecil

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -13,7 +13,7 @@
             </a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="{{ route('product.index') }}">
+        {{-- <a class="nav-link" href="{{ route('product.index') }}"> --}}
             <i class="nav-icon icon-drop"></i> produk
         </a>
     </li>
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,11 @@ use Illuminate\Http\Request;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'administrator', 'middleware' => 'auth'], function() {
     Route::get('/home', 'HomeController@index')->name('home'); //JADI ROUTING INI SUDAH ADA DARI ARTIKEL SEBELUMNYA TAPI KITA PINDAHKAN KEDALAM GROUPING
-
+    
     Route::resource('category', 'CategoryController')->except(['create', 'show']);
 
-   
 });
 
 
