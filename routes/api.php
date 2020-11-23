83a18a58a40070fb40854960b8ca2c922716c34e
icatpojan <irsyadfauzangg@gmail.com>
2020-11-23 21:40:01 +0700

    gini

--- routes/api.php
+++ routes/api.php
@@ -76,5 +76,5 @@ Route::get('gasorder', 'PaymentController@gasOrder')->middleware('jwt.verify');
 //ini buat ngambil data orderan yang ngorder barang kita
 Route::get('payment/{id}', 'PaymentController@show')->middleware('jwt.verify');
 //ini buat ngambil bukti pembayaran berdasarkan id order
-Route::post('send/{id}', 'PaymentController@send')->middleware('jwt.verify');
+Route::patch('send/{id}', 'PaymentController@send')->middleware('jwt.verify');
 //buat konfirmasi penjual kalo barang udah di kirim
