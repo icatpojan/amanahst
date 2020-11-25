4b691efaf3400139b2dae6c25dff82df776f969b
icatpojan <irsyadfauzangg@gmail.com>
2020-11-26 06:21:51 +0700

    gini

---
+++ app/Http/Controllers/Ecommerce/FrontController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Ecommerce;
+
+use App\Customer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+use App\Category;
+use App\Province;
+use App\Order;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+        //MEMBUAT QUERY UNTUK MENGAMBIL DATA PRODUK YANG DIURUTKAN BERDASARKAN TGL TERBARU
+        //DAN DI-LOAD 10 DATA PER PAGENYA
+        $products = Product::orderBy('created_at', 'DESC')->paginate(10);
+        //LOAD VIEW INDEX.BLADE.PHP DAN PASSING DATA DARI VARIABLE PRODUCTS
+        return view('ecommerce.index', compact('products'));
+    // return response()->json([
+    //     'yupak' => 'succes',
+    //     'data' => $products
+    // ]);
+    }
+    public function product()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
+        return view('ecommerce.product', compact('products'));
+        // return response()->json(compact('products'));
+    }
+    public function categoryProduct($slug)
+    {
+        //JADI QUERYNYA ADALAH KITA CARI DULU KATEGORI BERDASARKAN SLUG, SETELAH DATANYA DITEMUKAN
+        //MAKA SLUG AKAN MENGAMBIL DATA PRODUCT YANG BERELASI MENGGUNAKAN METHOD PRODUCT() YANG TELAH DIDEFINISIKAN PADA FILE CATEGORY.PHP SERTA DIURUTKAN BERDASARKAN CREATED_AT DAN DI-LOAD 12 DATA PER SEKALI LOAD
+        $products = Category::where('slug', $slug)->first()->product()->orderBy('created_at', 'DESC')->paginate(12);
+        //LOAD VIEW YANG SAMA YAKNI PRODUCT.BLADE.PHP KARENA TAMPILANNYA AKAN KITA BUAT SAMA JUGA
+        return view('ecommerce.product', compact('products'));
+    }
+    public function show($slug)
+    {
+        //QUERY UNTUK MENGAMBIL SINGLE DATA BERDASARKAN SLUG-NYA
+        $product = Product::with(['category'])->where('slug', $slug)->first();
+        //LOAD VIEW SHOW.BLADE.PHP DAN PASSING DATA PRODUCT
+        return view('ecommerce.show', compact('product'));
+    }
+    public function verifyCustomerRegistration($token)
+    {
+        //JADI KITA BUAT QUERY UNTUK MENGMABIL DATA USER BERDASARKAN TOKEN YANG DITERIMA
+        $customer = Customer::where('activate_token', $token)->first();
+        if ($customer) {
+            //JIKA ADA MAKA DATANYA DIUPDATE DENGNA MENGOSONGKAN TOKENNYA DAN STATUSNYA JADI AKTIF
+            $customer->update([
+                'activate_token' => null,
+                'status' => 1
+            ]);
+            //REDIRECT KE HALAMAN LOGIN DENGAN MENGIRIMKAN FLASH SESSION SUCCESS
+            return redirect(route('customer.login'))->with(['success' => 'Verifikasi Berhasil, Silahkan Login']);
+        }
+        //JIKA TIDAK ADA, MAKA REDIRECT KE HALAMAN LOGIN
+        //DENGAN MENGIRIMKAN FLASH SESSION ERROR
+        return redirect(route('customer.login'))->with(['error' => 'Invalid Verifikasi Token']);
+    }
+    public function customerSettingForm()
+    {
+        //MENGAMBIL DATA CUSTOMER YANG SEDANG LOGIN
+        $customer = auth()->guard('customer')->user()->load('district');
+        //GET DATA PROPINSI UNTUK DITAMPILKAN PADA SELECT BOX
+        $provinces = Province::orderBy('name', 'ASC')->get();
+        //LOAD VIEW setting.blade.php DAN PASSING DATA CUSTOMER - PROVINCES
+        return view('ecommerce.setting', compact('customer', 'provinces'));
+    }
+    public function customerUpdateProfile(Request $request)
+    {
+        //VALIDASI DATA YANG DIKIRIM
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'phone_number' => 'required|max:15',
+            'address' => 'required|string',
+            'district_id' => 'required|exists:districts,id',
+            'password' => 'nullable|string|min:6'
+        ]);
+
+        //AMBIL DATA CUSTOMER YANG SEDANG LOGIN
+        $user = auth()->guard('customer')->user();
+        //AMBIL DATA YANG DIKIRIM DARI FORM
+        //TAPI HANYA 4 COLUMN SAJA SESUAI YANG ADA DI BAWAH
+        $data = $request->only('name', 'phone_number', 'address', 'district_id');
+        //ADAPUN PASSWORD KITA CEK DULU, JIKA TIDAK KOSONG
+        if ($request->password != '') {
+            //MAKA TAMBAHKAN KE DALAM ARRAY
+            $data['password'] = $request->password;
+        }
+        //TERUS UPDATE DATANYA
+        $user->update($data);
+        //DAN REDIRECT KEMBALI DENGAN MENGIRIMKAN PESAN BERHASIL
+        return redirect()->back()->with(['success' => 'Profil berhasil diperbaharui']);
+    }
+    public function referalProduct($user, $product)
+    {
+        $code = $user . '-' . $product; //KITA MERGE USERID DAN PRODUCTID
+        $product = Product::find($product); //FIND PRODUCT BERDASARKAN PRODUCTID
+        $cookie = cookie('dw-afiliasi', json_encode($code), 2880); //BUAT COOKIE DENGAN NAMA DW-AFILIASI DAN VALUENYA ADALAH CODE YANG SUDAH DI-MERGE
+        //KEMUDIAN REDIRECT KE HALAMAN SHOW PRODUCT DAN MENGIRIMKAN COOKIE KE BROWSER
+        return redirect(route('front.show_product', $product->slug))->cookie($cookie);
+    }
+    public function listCommission()
+    {
+        $user = auth()->guard('customer')->user(); //AMBIL DATA USER YANG LOGIN
+        //QUERY BERDASARKAN ID USER DARI DATA REF YANG ADA DIORDER DENGAN STATUS 4 ATAU SELESAI
+        $orders = Order::where('ref', $user->id)->where('status', 4)->paginate(10);
+        //LOAD VIEW AFFILIATE.BLADE.PHP DAN PASSING DATA ORDERS
+        return view('ecommerce.affiliate', compact('orders'));
+    }
+}
---
+++ resources/views/ecommerce/index.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.ecommerce')
+
+@section('title')
+    <title>-SI AMANAH-</title>
+@endsection
+
+@section('content')
+    <!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="overlay"></div>
+		<div class="banner_inner d-flex align-items-center">
+			<div class="container">
+				<div class="banner_content row">
+					<div class="offset-lg-2 col-lg-8">
+						<h3>SI AMANAH
+							<br/>TEMPAT BELANJA AMAN DAN TERPERCAYA</h3>
+						<p>Menyediakan berbagai kebutuhan anda baik yang ada maupun tidak ada
+							semuanya diada ada agar 
+							menjadi ada. Agar semua puas, kamimenghadirkan dari berbagai suplier 
+							yang tidak dapat dipercaya
+						</p>
+						<a class="white_bg_btn" href="#">lihat koleksi</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Hot Deals Area =================-->
+	<section class="hot_deals_area section_gap">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="hot_deal_box">
+						<img class="img-fluid" src="{{ asset('ecommerce/img/product/hot_deals/deal1.jpg') }}" alt="">
+						<div class="content">
+							<h2>PRODUK TERBARU</h2>
+							<p>belanja sekarang</p>
+						</div>
+						<a class="hot_deal_link" href="#"></a>
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+					<div class="hot_deal_box">
+						<img class="img-fluid" src="{{ asset('ecommerce/img/product/hot_deals/deal1.jpg') }}" alt="">
+						<div class="content">
+							<h2>PRODUK TERBARU</h2>
+							<p>belanja sekarang</p>
+						</div>
+						<a class="hot_deal_link" href="#"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Hot Deals Area =================-->
+
+	<!--================Feature Product Area =================-->
+	<section class="feature_product_area section_gap">
+		<div class="main_box">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="main_title">
+						<h2>Produk Terbaru</h2>
+						<p>Tampil trendi dengan kumpulan produk kekinian kami.</p>
+					</div>
+				</div>
+				<div class="row">
+          
+          <!-- PERHATIAKAN BAGIAN INI, LOOPING DATA PRODUK -->
+          @forelse($products as $row)
+					<div class="col col1">
+						<div class="f_p_item">
+							<div class="f_p_img">
+				<!-- KEMUDIAN TAMPILKAN IMAGENYA DARI FOLDER /PUBLIC/STORAGE/PRODUCTS -->
+				<b style="color: white">{{ $row->image }}</b>
+				<img src="{{ $row->image }}" width="100px" height="100px" alt="{{ $row->name }}">
+								<div class="p_icon">
+									<a href="{{ url('/product/' . $row->slug) }}">
+										<i class="lnr lnr-cart"></i>
+									</a>
+								</div>
+							</div>
+              <!-- KETIKA PRODUK INI DIKLIK MAKA AKAN DIARAHKAN KE URL DIBAWAH -->
+              <!-- HANYA SAJA URL TERSEBUT BELUM DISEDIAKAN PADA ARTIKEL KALI INI -->
+              <a href="{{ url('/product/' . $row->slug) }}">
+                <!-- TAMPILKAN NAMA PRODUK -->
+                 <h4>{{ $row->name }}</h4>
+							</a>
+              <!-- TAMPILKAN HARGA PRODUK -->
+              <h5>Rp {{ number_format($row->price) }}</h5>
+						</div>
+					</div>
+          @empty
+                    
+          @endforelse
+				</div>
+
+        <!-- GENERATE PAGINATION UNTUK MEMBUAT NAVIGASI DATA SELANJUTNYA JIKA ADA -->
+				<div class="row">
+					{{ $products->links() }}
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Feature Product Area =================-->
+@endsection
