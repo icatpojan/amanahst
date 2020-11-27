a2ac7baf77d96710ccccd0253634edf490648382
icatpojan <irsyadfauzangg@gmail.com>
2020-11-27 14:20:25 +0700

    fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -19,16 +19,16 @@ class MessageController extends Controller
         // $users = User::where('id', '!=', Auth::id())->get();
 
         // count how many message are unread from the selected user
-        $users = DB::select("select users.id, users.name, users.avatar, users.email, count(is_read) as unread 
-        from users LEFT  JOIN  messages ON users.id = messages.from and is_read = 0 and messages.to = " . Auth::id() . "
-        where users.id != " . Auth::id() . " 
-        group by users.id, users.name, users.avatar, users.email");
-            // $Message = Message::with(['user:id,name,image'])->where('from', 'Auth::user()->id' || 'to', 'Auth::user()->id')->get();
+        // $users = DB::select("select users.id, users.name, users.avatar, users.email, count(is_read) as unread 
+        // from users LEFT  JOIN  messages ON users.id = messages.from and is_read = 0 and messages.to = " . Auth::id() . "
+        // where users.id != " . Auth::id() . " 
+        // group by users.id, users.name, users.avatar, users.email");
+            $Message = Message::with(['user:id,name,image'])->where('from', 'Auth::user()->id' || 'to', 'Auth::user()->id')->get();
 
         // return view('home', ['users' => $users]);
         return response()->json([
-            // $Message
-            $users
+            $Message
+            // $users
         ]);
     }
 
