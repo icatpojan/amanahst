283fc3b2725e6931925922b0ba67be718c9b4569
icatpojan <irsyadfauzangg@gmail.com>
2020-11-27 20:30:41 +0700

    fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -26,7 +26,7 @@ class MessageController extends Controller
 
 
 
-        $my_id = Auth::id();
+        $my_id = Auth::user()->id;
         dd($my_id);
         $users = DB::select("select users.id, users.name, users.avatar, users.email, count(is_read) as unread FROM users LEFT  JOIN  messages ON " . $my_id ." = messages.from AND is_read = 0 AND messages.to = " . $my_id . " WHERE users.id != " . $my_id . " GROUP BY users.id, users.name, users.avatar, users.email");
         // $Message = Message::with(['user:id,name,image', 'pesan:id,name,image'])->where('from', Auth::user()->id)->where('to', Auth::user()->id)->get();
