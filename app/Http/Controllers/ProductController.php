de40ccfa91ef82c0a109f0ca4f9416511ece6d6e
icatpojan <irsyadfauzangg@gmail.com>
2020-11-30 17:02:29 +0700

    search

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,7 +43,7 @@ class ProductController extends Controller
     {
 
         $search = $request->get('search');
-        $product = Product::whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($search) . '%'])->orWhereRaw('LOWER(description) LIKE ?', ['%' . strtolower($search) . '%'])->get();
+        $product = Product::with(['user'])->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($search) . '%'])->orWhereRaw('LOWER(description) LIKE ?', ['%' . strtolower($search) . '%'])->get();
         if (($product)->isEmpty()) {
 
             return $this->sendResponse('Error', 'tidak ada data yang namanya kayak gitu', null, 500);
