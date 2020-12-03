0788f2694bcc0527be92a9e3958c83040b9da1b3
icatpojan <irsyadfauzangg@gmail.com>
2020-12-03 16:18:27 +0700

    dashboard

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','alamat','image','nomor_telpon','kode',
+        'name', 'email', 'password','alamat','image','nomor_telpon','kode','role',
     ];
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,47 +9,54 @@
 
 <!-- TAG YANG DIAPIT OLEH SECTION('CONTENT') AKAN ME-REPLACE @YIELD('CONTENT') PADA MASTER LAYOUTS -->
 @section('content')
-<main class="main">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item">Home</li>
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol>
-    <div class="container-fluid">
-        <div class="animated fadeIn">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">perkembangan</h4>
+    <main class="main">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">Home</li>
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+        <div class="container-fluid">
+            <div class="animated fadeIn">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="jumbotron jumbotron-fluid">
+                            <div class="container">
+                                <h1 class="display-4">SELAMATA DATANG BOS</h1>
+                                <p class="lead">semua berjalan dengan normal walau hati sedang kacau dan kesepian</p>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="callout callout-info">
-                                        <small class="text-muted">jumlah uang masuk</small>
-                                        <br>
-                                        <strong class="h4">{{ number_format($Payment) }}</strong>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">perkembangan</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="callout callout-info">
+                                            <small class="text-muted">jumlah uang masuk</small>
+                                            <br>
+                                            <strong class="h4">{{ number_format($Payment) }}</strong>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="callout callout-danger">
-                                        <small class="text-muted">Jumlah member</small>
-                                        <br>
-                                        <strong class="h4">{{ number_format($User) }}</strong>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-danger">
+                                            <small class="text-muted">Jumlah member</small>
+                                            <br>
+                                            <strong class="h4">{{ number_format($User) }}</strong>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="callout callout-primary">
-                                        <small class="text-muted">Perlu Dikirim</small>
-                                        <br>
-                                        <strong class="h4">{{ number_format($Order) }}</strong>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-primary">
+                                            <small class="text-muted">Perlu Dikirim</small>
+                                            <br>
+                                            <strong class="h4">{{ number_format($Order) }}</strong>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="callout callout-success">
-                                        <small class="text-muted">Total Produk</small>
-                                        <br>
-                                        <strong class="h4">{{ number_format($Product) }}</strong>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-success">
+                                            <small class="text-muted">Total Produk</small>
+                                            <br>
+                                            <strong class="h4">{{ number_format($Product) }}</strong>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -58,6 +65,5 @@
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 @endsection
