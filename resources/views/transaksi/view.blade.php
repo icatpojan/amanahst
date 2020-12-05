3c591c83466eedcc82696ebb54f7043501bc317f
icatpojan <irsyadfauzangg@gmail.com>
2020-12-05 09:10:54 +0700

    db

--- resources/views/transaksi/view.blade.php
+++ resources/views/transaksi/view.blade.php
@@ -5,28 +5,31 @@
 @endsection
 
 @section('content')
-<main class="main">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item">Home</li>
-        <li class="breadcrumb-item active">View Order</li>
-    </ol>
-    <div class="container-fluid">
-        <div class="animated fadeIn">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">
-                                Detail pesanan
-                            </h4>
-                            @foreach ($order_details as $order_detail)
-                                {{ $order_detail->product_id }}
-                            @endforeach
+    <main class="main">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">Home</li>
+            <li class="breadcrumb-item active">View Order</li>
+        </ol>
+        <div class="container-fluid">
+            <div class="animated fadeIn">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">
+                                    Detail pesanan
+                                </h4>
+
+                                @foreach ($order_details as $order_detail)
+                                    {{ $order_detail->order_id }}
+                                    {{ $order_detail->jumlah }}
+                                    {{ $order_detail->jumlah_harga }}
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 @endsection
